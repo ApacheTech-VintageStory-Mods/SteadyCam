3749f588c84050b279239882953111ad882e9e62
ApacheTech Solutions <p_matthews@live.co.uk>
2024-11-24 16:57:17 +0000

    feat: update project files and settings

--- src/SteadyCam/Program.cs
+++ src/SteadyCam/Program.cs
@@ -1,34 +1,5 @@
-﻿using ApacheTech.Common.DependencyInjection.Abstractions;
-using Gantry.Core.Hosting;
-using Gantry.Services.HarmonyPatches.Hosting;
-using JetBrains.Annotations;
-using Vintagestory.API.Common;
+﻿
 
 namespace ApacheTech.VintageMods.SteadyCam;
 
-/// <summary>
-///     Entry-point for the mod. This class will configure and build the IOC Container, and Service list for the rest of the mod.
-///     
-///     Registrations performed within this class should be global scope; by convention, features should aim to be as stand-alone as they can be.
-/// </summary>
-/// <remarks>
-///     Only one derived instance of this class should be added to any single mod within
-///     the VintageMods domain. This class will enable Dependency Injection, and add all
-///     the domain services. Derived instances should only have minimal functionality, 
-///     instantiating, and adding Application specific services to the IOC Container.
-/// </remarks>
-/// <seealso cref="ModHost" />
-[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
-internal sealed class Program() : ModHost(
-#if DEBUG
-    debugMode: true
-#else
-    debugMode: false
-#endif
-)
-{
-    protected override void ConfigureUniversalModServices(IServiceCollection services, ICoreAPI api)
-    {
-        services.AddHarmonyPatchingService(api, o => o.AutoPatchModAssembly = true);
-    }
-}
+internal sealed class Program : ModHost;
---
+++ src/SteadyCam/Properties/GlobalUsings.cs
@@ -0,0 +1,28 @@
+﻿global using ApacheTech.Common.DependencyInjection.Abstractions;
+global using ApacheTech.Common.DependencyInjection.Abstractions.Extensions;
+global using ApacheTech.Common.Extensions.Harmony;
+global using ApacheTech.Common.Extensions.System;
+
+global using Gantry.Core;
+global using Gantry.Core.Extensions;
+global using Gantry.Core.Extensions.Api;
+global using Gantry.Core.GameContent.ChatCommands.Parsers.Extensions;
+global using Gantry.Core.GameContent.Extensions;
+global using Gantry.Core.Hosting;
+global using Gantry.Core.Hosting.Registration;
+global using Gantry.Core.ModSystems;
+global using Gantry.Services.FileSystem.Hosting;
+global using Gantry.Services.HarmonyPatches.Annotations;
+
+global using HarmonyLib;
+
+global using JetBrains.Annotations;
+
+global using Vintagestory.API.Client;
+global using Vintagestory.API.Common;
+global using Vintagestory.API.Common.Entities;
+global using Vintagestory.API.MathTools;
+global using Vintagestory.Client.NoObf;
+
+global using System;
+global using System.Drawing;
